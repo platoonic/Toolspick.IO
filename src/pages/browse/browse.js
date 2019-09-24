de8d0001828cab488dd13691c6e4bce8311c6646
coderizer <coderizer@aol.com>
2019-09-24 21:03:40 +0200

    Create view package dynamic component

--- src/pages/browse/browse.js
+++ src/pages/browse/browse.js
@@ -26,56 +26,88 @@ class Browse extends React.Component{
 						name: 'Express',
 						description: 'Web framework for Node.JS',
 						stability: 90,
-						activity: 80
+						activity: 80,
+						type: 'Backend Framework'
 					},
 					{
 						id: 1,
 						name: 'Flutter',
 						description: 'Hybrid Mobile application development framework',
 						stability: 80,
-						activity: 70
+						activity: 70,
+						type: 'Mobile development Framework'
 					},
 					{
 						id: 2,
 						name: 'Redis',
 						description: 'Caching System and Message broker',
 						stability: 90,
-						activity: 80
+						activity: 80,
+						type: 'Caching Library'
 					},
 					{
 						id: 3,
 						name: 'Angular.JS',
 						description: 'Frontend web development framework',
 						stability: 60,
-						activity: 80
+						activity: 80,
+						type: 'Frontend Framework'
 					},
 					{
 						id: 4,
 						name: 'Loopback',
 						description: 'Backend NodeJS web framework',
 						stability: 27,
-						activity: 75
+						activity: 75,
+						type: 'Backend Framework'
 					},
 					{
 						id: 5,
 						name: 'Vue.JS',
 						description: 'Frontend web development framework',
 						stability: 86,
-						activity: 50
+						activity: 50,
+						type: 'Frontend Framework'
 					},
 					{
 						id: 6,
 						name: 'Tensorflow',
 						description: 'Machine learning high-level API library',
 						stability: 86,
-						activity: 80
+						activity: 80,
+						type: 'Machine Learning Library'
 					},
 					{
 						id: 7,
 						name: 'Axios',
 						description: 'Javascript HTTP requests management library',
 						stability: 70,
-						activity: 60
+						activity: 60,
+						type: 'JS Library'
+					},
+					{
+						id: 8,
+						name: 'Docker',
+						description: 'Deployment system that utilizes virtual containers',
+						stability: 80,
+						activity: 67,
+						type: 'Deployment tool'
+					},
+					{
+						id: 9,
+						name: 'GraphQL',
+						description: 'Advanced API library more advanced than REST',
+						stability: 80,
+						activity: 70,
+						type: 'API'
+					},
+					{
+						id: 10,
+						name: 'React Native',
+						description: 'Hybrid mobile development framework',
+						stability: 80,
+						activity: 80,
+						type: 'Mobile development framework'
 					}
 				]})
 		}, 1000)
--- src/pages/view/view.js
+++ src/pages/view/view.js
@@ -7,6 +7,20 @@ import {Line, Bar} from 'react-chartjs-2'
 class View extends React.Component{
 	constructor(props){
 		super(props)
+		this.state = {
+			loadingData:true,
+			toolData:{
+				name: 'Express',
+				description: 'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.',
+				stability: 30,
+				activity: 90,
+				dependencies: 2,
+				sofQuestions: 5123,
+				githubStars: 12321,
+				githubContributors: 92,
+				usedBy: 22132
+			}
+		}
 		this.charts = {
 			stackoverflowData: {
 			    labels: [2015,2016,2017,2018,2019],
@@ -65,6 +79,7 @@ class View extends React.Component{
 				}
 			}
 		}
+		this.loading = null;
 		//Stackoverflow options object
 		this.sofOptions = JSON.parse(JSON.stringify(this.charts.options));
 		this.sofOptions.title.text = "Number of Stackoverflow questions";
@@ -75,69 +90,97 @@ class View extends React.Component{
 		this.frequencyOptions = JSON.parse(JSON.stringify(this.charts.options));
 		this.frequencyOptions.title.text = "Versions Frequency";
 	}
+
+	componentDidMount(){
+		this.loading = setTimeout(() => {this.setState({ loading: false })}, 1000);
+	}
+
 	render(){
-		return(
-			<div className="row">
-				<div className="list single">
-					<ToolsBar page="view"/>
-					<div className="items single">
-						<div className="row"> 
-							<div className="column4">
-								<div className="data">
-									<h4>Express</h4>
-									<div className="description">
-										<span className="description">Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.</span>
-									</div>
-									<div className="stats">
-										<div className="stat">
-											<span className="title">Stability: </span>
-											<div className="meter">
-												<span className="low" style={{width:"30%"}}></span>
+		const loadingBlocks = <div className="row">
+								<div className="list single loading">
+							  		<ToolsBar page="view"/>
+							  		<div className="loadingTool">
+							  			<div className="column4">
+							  				<div className="gradient data"></div>
+							  				<div className="gradient data"></div>
+							  			</div>
+							  			<div className="column4">
+							  				<div className="gradient data"></div>
+							  				<div className="gradient data"></div>
+							  			</div>
+							  			<div className="column4">
+							  				<div className="gradient data"></div>
+							  			</div>
+							  		</div>
+							  	</div>
+							  </div>;
+		if(this.state.loading === false){
+			let tool = this.state.toolData;
+			return(
+				<div className="row">
+					<div className="list single">
+						<ToolsBar page="view"/>
+						<div className="items single">
+							<div className="row"> 
+								<div className="column4">
+									<div className="data">
+										<h2>{tool.name}</h2>
+										<div className="description">
+											<span className="description">{tool.description}</span>
+										</div>
+										<div className="stats">
+											<div className="stat">
+												<span className="title">Stability: </span>
+												<div className="meter">
+													<span className="low" style={{width:"30%"}}></span>
+												</div>
+											</div>
+											<div className="stat">
+												<span className="title">Activity:  </span>
+												<div className="meter">
+													<span className="high" style={{width:"90%"}}></span>
+												</div>
 											</div>
 										</div>
-										<div className="stat">
-											<span className="title">Activity:  </span>
-											<div className="meter">
-												<span className="high" style={{width:"90%"}}></span>
+										<div className="metrics">
+											<div className="metric">
+												<i className="fa fa-archive"></i>
+												<span>Dependencies: <span className="bold">{tool.dependencies}</span></span>
 											</div>
-										</div>
-									</div>
-									<div className="metrics">
-										<div className="metric">
-											<i className="fa fa-archive"></i>
-											<span>Dependencies: <span className="bold">2</span></span>
-										</div>
-										<div className="metric">
-											<i className="fa fa-question-circle"></i>
-											<span>Stackoverflow Questions (2019): <span className="bold">5123</span></span>
-										</div>
-										<div className="metric">
-											<i className="fa fa-star"></i>
-											<span>Github stars: <span className="bold">12,312</span></span>
-										</div>
-										<div className="metric">
-											<i className="fa fa-users"></i>
-											<span>Github contributors: <span className="bold">192</span></span>
-										</div>
-										<div className="metric">
-											<i className="fa fa-cubes"></i>
-											<span>Used by: <span className="bold">2,123 repository</span></span>
+											<div className="metric">
+												<i className="fa fa-question-circle"></i>
+												<span>Stackoverflow Questions (2019): <span className="bold">{tool.sofQuestions}</span></span>
+											</div>
+											<div className="metric">
+												<i className="fa fa-star"></i>
+												<span>Github stars: <span className="bold">{tool.githubStars}</span></span>
+											</div>
+											<div className="metric">
+												<i className="fa fa-users"></i>
+												<span>Github contributors: <span className="bold">{tool.githubContributors}</span></span>
+											</div>
+											<div className="metric">
+												<i className="fa fa-cubes"></i>
+												<span>Used by: <span className="bold">{tool.usedBy} repository</span></span>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-							<div className="column4">
-								<Line width={300} height={220} data={this.charts.stackoverflowData} options={this.sofOptions}/>
-								<Bar width={300} height={220} data={this.charts.versionsData} options={this.frequencyOptions}/>
-							</div>
-							<div className="column4">
-								<Line width={300} height={220} data={this.charts.googletrendsData} options={this.googleTrendsOptions}/>
+								<div className="column4">
+									<Line width={300} height={220} data={this.charts.stackoverflowData} options={this.sofOptions}/>
+									<Bar width={300} height={220} data={this.charts.versionsData} options={this.frequencyOptions}/>
+								</div>
+								<div className="column4">
+									<Line width={300} height={220} data={this.charts.googletrendsData} options={this.googleTrendsOptions}/>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		)
+			)
+		}else{
+			return(loadingBlocks);
+		}
 	}
 }
 
